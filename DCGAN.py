630b22569e4c51c57c9faa0ca0f0ab108d5f4afb
Roy Tseng <roytseng.tw@gmail.com>
2017-03-28 09:39:17 +0800

    undo fix step count

--- DCGAN.py
+++ DCGAN.py
@@ -25,17 +25,17 @@ class DCGAN():
         if F.is_training:
             self.epoch_id = 1
             self.batch_id = 1
-
+            
             # We expect no remainders, to make the calculated `batch_id` precise
-            self.n_batch = F.train_size // F.batch_size
-
+            self.n_batch = F.train_size // F.batch_size  
+            
             # width param to format verbose information
             self.batch_width = len(str(self.n_batch))
             if F.training_strategy == 1:
-                self.step_width = len(str(F.epoch * self.n_batch))
+                self.step_width = len(str(F.epoch * self.n_batch * (1 + F.g_step / F.d_step)))
             elif F.training_strategy == 2:
                 self.step_width = len(str(F.epoch * self.n_batch * (F.d_step + F.g_step)))
-
+    
     def __call__(self, input, model_name=None, reuse=False):
         if not model_name:
             model_name = self.__class__.__name__
@@ -43,23 +43,23 @@ class DCGAN():
             self.input = input
             output = self.setup_model(input)
         return output
-
+    
     @property
     def vars_all(self):
-        return tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES,
+        return tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, 
                                  scope=self.model_scope.name)
-
+    
     @property
     def vars_train(self):
-        return tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES,
+        return tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, 
                                  scope=self.model_scope.name)
-
+    
     @property
     def logging_tensors(self):
         return {'step': tf.train.get_global_step(),
                 'd_loss': self.d_loss,
                 'g_loss': self.g_loss}
-
+    
     def logging_formatter(self, results):
         '''
         Display verbose training information on the terminal.
@@ -68,10 +68,10 @@ class DCGAN():
         message = ('[Epoch {:02d}] [{:{batch_width}d} / {:d}] [Step {:{step_width}d}]'
                    '  D_loss: {:.5f}, G_loss: {:.5f}'
                    .format(self.epoch_id, self.batch_id, self.n_batch, results['step'],
-                           results['d_loss'], results['g_loss'],
+                           results['d_loss'], results['g_loss'], 
                            batch_width=self.batch_width, step_width=self.step_width))
         return message
-
+    
     def setup_model(self, input):
         '''
         Args:
@@ -85,9 +85,9 @@ class DCGAN():
         d_out_real, d_logit_real = self.D(input['im_gt'])
         d_out_fake, d_logit_fake = self.D(g_out, reuse=True)
         self.output = g_out
-
+        
         self.sample_img = self.G(input['sample_z'], reuse=True)
-
+        
         ###################
         # (2) Define loss #
         ###################
@@ -100,13 +100,13 @@ class DCGAN():
         g_loss_fake = tf.reduce_mean(
             tf.nn.sigmoid_cross_entropy_with_logits(labels=tf.ones_like(d_out_fake),
                                                     logits=d_logit_fake))
-
+        
         d_reg_loss = tcl.apply_regularization(self.regularizer, weights_list=self.D.vars_train)
         g_reg_loss = tcl.apply_regularization(self.regularizer, weights_list=self.G.vars_train)
-
+        
         self.d_loss = d_loss_real + d_loss_fake + d_reg_loss
         self.g_loss = g_loss_fake + g_reg_loss
-
+        
         ########################
         # (3) Define optimizer #
         ########################
@@ -119,7 +119,7 @@ class DCGAN():
         self.g_train_op = tcl.optimize_loss(
             loss=self.g_loss, optimizer=g_optimizer, learning_rate=F.learning_rate,
             variables=self.G.vars_train, global_step=global_step, name='g_optim')
-
+        
         ######################
         # (4) Define summary #
         ######################
@@ -136,24 +136,24 @@ class DCGAN():
         tf.summary.image('real', input['im_gt'], max_outputs=3)
         # merge all summary operations to a single operation
         self.summary_all = tf.summary.merge_all()
-
+         
         return self.output
-
+    
     def train_iter(self, sess, feed_dict=None):
         F = self.F
         for i in range(F.d_step):
             _= sess.run([self.d_train_op], feed_dict=feed_dict)
-
+            
             if F.training_strategy == 1:
                 # increase batch_id and maybe epoch_id
                 self.batch_id += 1
                 if self.batch_id > self.n_batch:
                     self.batch_id = 1
                     self.epoch_id += 1
-
+        
         for i in range(F.g_step):
             _ = sess.run([self.g_train_op], feed_dict=feed_dict)
-
+            
         if F.training_strategy == 2:
             # increase batch_id and maybe epoch_id
             self.batch_id += 1
@@ -170,7 +170,7 @@ class SampleImageHook(tf.train.SessionRunHook):
           sample_img : `Tensor`, sample images to save.
           img_path: 'String', path containing the directory and filename prefix
           every_n_iter: `int`, save the sample images every N local steps.
-          every_n_secs: `int` or `float`, save sample images every N seconds.
+          every_n_secs: `int` or `float`, save sample images every N seconds. 
                 Exactly one of `every_n_iter` and `every_n_secs` should be provided.
         '''
         self.model = model
@@ -217,4 +217,4 @@ class SampleImageHook(tf.train.SessionRunHook):
             #             run_values.results['gt_img'][:64],
             #             self.grid_size)
         self._iter_count += 1
-
+            
